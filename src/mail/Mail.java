01cd4fc285da0db0a68fad1ae53bff20ab5ddc67
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-08-15 02:40:13 -0700

    increment

--- src/mail/Mail.java
+++ src/mail/Mail.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mail;
 
-/**
- *
- * @author thufir
- */
 public class Mail {
 
     /**
